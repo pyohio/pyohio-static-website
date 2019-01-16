4a4bfc5c564796edd31130d286f4ec22788794a8
Dave Forgac <tylerdave@tylerdave.com>
2019-01-15 23:50:33 -0500

    fix previews

--- src/cms/cms.js
+++ src/cms/cms.js
@@ -4,6 +4,6 @@ import GeneralPagePreview from './preview-templates/GeneralPagePreview'
 import NewsPostPreview from './preview-templates/NewsPostPreview'
 import ProductPagePreview from './preview-templates/ProductPagePreview'
 
-CMS.registerPreviewTemplate('about', GeneralPagePreview)
+CMS.registerPreviewTemplate('general', GeneralPagePreview)
 CMS.registerPreviewTemplate('products', ProductPagePreview)
 CMS.registerPreviewTemplate('news', NewsPostPreview)
