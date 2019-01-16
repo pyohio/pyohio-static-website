44b354e2db1dc6a00edb05226eaa6e24bf5f9092
Dave Forgac <tylerdave@tylerdave.com>
2019-01-16 01:08:45 -0500

    register all general pages

--- src/cms/cms.js
+++ src/cms/cms.js
@@ -4,6 +4,13 @@ import GeneralPagePreview from './preview-templates/GeneralPagePreview'
 import NewsPostPreview from './preview-templates/NewsPostPreview'
 import ProductPagePreview from './preview-templates/ProductPagePreview'
 
-CMS.registerPreviewTemplate('general', GeneralPagePreview)
+
+
 CMS.registerPreviewTemplate('products', ProductPagePreview)
 CMS.registerPreviewTemplate('news', NewsPostPreview)
+
+const generalPages = ['about', 'home', 'sponsorship'];
+
+generalPages.forEach(function (page) {
+    CMS.registerPreviewTemplate(page, GeneralPagePreview);
+});
