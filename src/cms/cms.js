c646a19f127107b7df3dc152190c0abc7aa7bf0a
Dave Forgac <tylerdave@tylerdave.com>
2019-02-05 11:00:01 -0500

    remove product page

--- src/cms/cms.js
+++ src/cms/cms.js
@@ -2,11 +2,7 @@ import CMS from 'netlify-cms'
 
 import GeneralPagePreview from './preview-templates/GeneralPagePreview'
 import NewsPostPreview from './preview-templates/NewsPostPreview'
-import ProductPagePreview from './preview-templates/ProductPagePreview'
 
-
-
-CMS.registerPreviewTemplate('products', ProductPagePreview)
 CMS.registerPreviewTemplate('news', NewsPostPreview)
 
 const generalPages = ['about', 'home', 'sponsorship'];
