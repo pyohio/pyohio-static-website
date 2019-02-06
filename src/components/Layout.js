4a385351a224e7caae055da15c82164eb154a766
Dave Forgac <tylerdave@tylerdave.com>
2019-02-05 19:43:41 -0500

    hardcode year url for now

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -24,17 +24,17 @@ const TemplateWrapper = ({ children }) => (
           <title>{data.site.siteMetadata.title}</title>
           <meta name="description" content={data.site.siteMetadata.description} />
           
-          <link rel="apple-touch-icon" sizes="180x180" href="/img/apple-touch-icon.png" />
-	        <link rel="icon" type="image/png" href="/img/favicon-32x32.png" sizes="32x32" />
-	        <link rel="icon" type="image/png" href="/img/favicon-16x16.png" sizes="16x16" />
+          <link rel="apple-touch-icon" sizes="180x180" href="/2019/img/apple-touch-icon.png" />
+	        <link rel="icon" type="image/png" href="/2019/img/favicon-32x32.png" sizes="32x32" />
+	        <link rel="icon" type="image/png" href="/2019/img/favicon-16x16.png" sizes="16x16" />
 	
-	        <link rel="mask-icon" href="/img/safari-pinned-tab.svg" color="#ff4400" />
+	        <link rel="mask-icon" href="/2019/img/safari-pinned-tab.svg" color="#ff4400" />
 	        <meta name="theme-color" content="#fff" />
 
 	        <meta property="og:type" content="business.business" />
           <meta property="og:title" content={data.site.siteMetadata.title} />
-          <meta property="og:url" content="/" />
-          <meta property="og:image" content="/img/og-image.jpg" />
+          <meta property="og:url" content="/2019/" />
+          <meta property="og:image" content="/2019/img/og-image.jpg" />
         </Helmet>
         <Navbar />
         <div>{children}</div>
