40c86fcd300bb3d711c0dcda18407cb6b543199a
Dave Forgac <tylerdave@tylerdave.com>
2019-02-06 23:15:54 -0500

    test rich snippet

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -38,6 +38,39 @@ const TemplateWrapper = ({ children }) => (
         </Helmet>
         <Navbar />
         <div>{children}</div>
+        <script type="application/ld+json">
+{
+  "@context": "https://schema.org",
+  "@type": "Event",
+  "name": "PyOhio 2019",
+  "startDate": "2019-07-27T10:00-05:00",
+  "location": {
+    "@type": "Place",
+    "name": "The Ohio Union",
+    "address": {
+      "@type": "PostalAddress",
+      "streetAddress": "1739 N. High St",
+      "addressLocality": "Columbus",
+      "postalCode": "43210",
+      "addressRegion": "OH",
+      "addressCountry": "US"
+    }
+  },
+  "image": [
+    "https://www.pyohio.org/2019/img/og-image.png"
+   ],
+  "description": "The FREE Python community conference in Columbus, OH, July 27-28, 2019.",
+  "endDate": "2019-07-28T17:00-05:00",
+  "offers": {
+    "@type": "Offer",
+    "url": "https://www.pyohio.org/2019/",
+    "price": "0",
+    "priceCurrency": "USD",
+    "availability": "https://schema.org/InStock",
+    "validFrom": "2019-04-15T09:00-05:00"
+  }
+}
+</script>
       </div>
     )}
   />
