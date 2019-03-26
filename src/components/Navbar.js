0fbda83274f2d233af6c7751ae0b2f8d6cc1c24c
Kat Passen <same@kpassen.com>
2019-03-26 12:07:20 -0400

    Make the hamburger button an actual button (#13)
    * update hamburger button to be an accessible button
    
    * not doing the work necessary for haspopup, so removing it

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -5,46 +5,38 @@ import logo from '../img/pyohio-logo-small.png'
 
 const Navbar = class extends React.Component {
 
-  componentDidMount() {
-    // Get all "navbar-burger" elements
-   const $navbarBurgers = Array.prototype.slice.call(document.querySelectorAll('.navbar-burger'), 0);
-    // Check if there are any navbar burgers
-   if ($navbarBurgers.length > 0) {
- 
-     // Add a click event on each of them
-     $navbarBurgers.forEach( el => {
-       el.addEventListener('click', () => {
- 
-         // Get the target from the "data-target" attribute
-         const target = el.dataset.target;
-         const $target = document.getElementById(target);
- 
-         // Toggle the "is-active" class on both the "navbar-burger" and the "navbar-menu"
-         el.classList.toggle('is-active');
-         $target.classList.toggle('is-active');
- 
-       });
-     });
-   }
- }
+  constructor(props) {
+    super(props);
+    this.state = {
+      burgerExpanded: false
+    };
+
+    this.handleClick = this.handleClick.bind(this);
+  }
+
+  handleClick() {
+    this.setState(state => ({
+      burgerExpanded: !state.burgerExpanded
+    }));
+  }
  
  render() {
    return (
   
-  <nav className="navbar is-transparent" role="navigation" aria-label="main-navigation">
+  <nav className="navbar is-transparent" role="navigation" aria-label="main navigation">
     <div className="container">
       <div className="navbar-brand">
         <Link to="/" className="navbar-item" title="Logo">
           <img src={logo} alt="PyOhio 2019"  />
         </Link>
         {/* Hamburger menu */}
-        <div className="navbar-burger burger" data-target="navMenu">
+        <button className={this.state.burgerExpanded ? "navbar-burger burger is-active" : "navbar-burger burger"} data-target="navMenu" onClick={this.handleClick} aria-label="hamburger menu" aria-expanded={this.state.burgerExpanded ? true : false}>
           <span></span>
           <span></span>
           <span></span>
-        </div>
+        </button>
       </div>
-      <div id="navMenu" className="navbar-menu">
+      <div id="navMenu" className={this.state.burgerExpanded ? "navbar-menu is-active" : "navbar-menu"}>
       <div className="navbar-start has-text-centered">
         <Link className="navbar-item" to="/about">
           About
