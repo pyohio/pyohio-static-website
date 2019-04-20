d7a76acb74a9423b6781716c4806c276a4826a58
Kat Passen <same@kpassen.com>
2019-04-19 22:01:44 -0400

    Add sponsor info to homepage and footer (#37)
    * add disabled footer
    
    * make the premiere sponsor footer happen
    
    * convert back to premier and disable footer
    
    * show at least some footer; conditionally render the rest
    
    * start of sponsor showcase layout (comit is borked)
    
    * make the sponsor showcase actually work and align with the rest of the homepage
    
    * reindent footer.js

---
+++ src/components/Footer.js
@@ -0,0 +1,56 @@
+import React from 'react'
+import { StaticQuery, graphql } from 'gatsby'
+import Img from 'gatsby-image'
+
+const Footer = () => (
+  <StaticQuery
+    query={graphql`
+    query sponsorsQuery {
+      allSponsorLevels(filter: {name: {eq: "Premier"}}) {
+        edges {
+          node {
+          name
+            sponsors {
+              name
+              web_logo {
+                local {
+                  childImageSharp{
+                    resize(height: 50) {
+                      src
+                      tracedSVG
+                      width
+                      height
+                      aspectRatio
+                      originalName
+                    }
+                  }
+                }
+              }
+            }
+          }
+        }
+      }
+    }
+    `}
+    render={
+      data => (
+        <footer className="footer">
+          <div className="content has-text-centered">
+            <p>&copy; 2019 PyOhio</p>
+            {data.allSponsorLevels.edges[0].node.sponsors != null &&
+                <div>
+                <p>PyOhio is made possible by our premier sponsor:</p>
+                <Img fixed={data.allSponsorLevels.edges[0].node.sponsors[0].web_logo.local.childImageSharp.resize}
+                alt={data.allSponsorLevels.edges[0].node.sponsors[0].name}
+                />
+                <p>and our other sponsors.</p>
+                </div>
+            }
+          </div>
+        </footer>
+      )
+    }
+  />
+)
+
+export default Footer
---
+++ src/components/SponsorShowcase.js
@@ -0,0 +1,68 @@
+import React from 'react'
+import { StaticQuery, graphql } from 'gatsby'
+import Img from "gatsby-image"
+
+export default () => (
+  <StaticQuery
+    query={graphql`
+      query SponsorsShowcaseQuery {
+        allSponsorLevels(
+          sort: {fields:order}
+          filter: {order: {lt: 100}}
+        ) {
+          edges {
+            node {
+              id
+              name
+              cost
+              order
+              sponsors {
+                id
+                name
+                description
+                url
+                twitter
+                web_logo {
+                  local {
+                    childImageSharp {
+                      fixed(width:250) {
+                        ...GatsbyImageSharpFixed
+                      }
+                    }
+                  }
+                }
+              }
+            }
+          }
+        }
+      }
+    `}
+    render={
+      data => (
+        <section className="section">
+          <div className="container">
+            <div className="columns">
+              <div className="column is-10 is-offset-1">
+                <div className="section">
+                  <h2 className="has-text-weight-bold is-size-2">Sponsors</h2>
+                  <div className="content sponsor-showcase-container">
+                  {data.allSponsorLevels.edges.filter(level => level.node.sponsors)
+                    .map(({ node: level }) => (
+                        level.sponsors.map((sponsor, index) => (
+                          <div className="sponsor" key={index}>
+                              <a href={sponsor.url}>
+                                  <Img fixed={sponsor.web_logo.local.childImageSharp.fixed}  alt={sponsor.web_logo.description}/>
+                              </a>
+                          </div>
+                        ))
+                    ))}
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </section>
+      )
+    }
+  />
+)
