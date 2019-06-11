d20944c4878e318cdf0cda31561d24958e87a7cf
Dave Forgac <tylerdave@tylerdave.com>
2019-06-11 00:45:57 -0400

    make premier sponsor more prominent

--- gatsby-config.js
+++ gatsby-config.js
@@ -125,6 +125,7 @@ module.exports = {
       options: {
         develop: true,            // Activates purging in npm run develop
         purgeOnly: ['/all.sass'], // applies purging only on the bulma css file
+        printRejected: true,
       },
     }, // must be after other CSS plugins
     'gatsby-plugin-netlify', // make sure to keep it last in the array
