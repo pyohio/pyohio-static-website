2390a2ce45b82c240cb7e53884942175e8d77111
Kat Passen <same@kpassen.com>
2019-07-01 16:11:22 -0400

    shut eslint up

--- src/components/ScheduleSlot.js
+++ src/components/ScheduleSlot.js
@@ -1,6 +1,5 @@
 import React from "react"
 import { Link } from "gatsby"
-import strftime from "strftime"
 
 export default class ScheduleSlot extends React.Component {
   constructor(props) {
@@ -8,19 +7,6 @@ export default class ScheduleSlot extends React.Component {
     this.slot = props.slot;
   }
   render() {
-    function formatDatetime(timeString, format) {
-      let formatted = ""
-      const datetime = new Date(timeString)
-      if (!isNaN(datetime)) {
-        formatted = strftime(format, datetime)
-      }
-      return formatted
-    }
-
-    function formatTime(timeString) {
-      return formatDatetime(timeString, "%-I:%M%P")
-    }
-
     return (
       <div className={`card schedule-item ${this.slot.kind === 'talk' ? 'schedule-talk' : 'schedule-non-talk'}`}>
         <div className="card-content is-flex">
