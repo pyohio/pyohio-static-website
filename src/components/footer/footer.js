0a5ec2cf0fc082469c9bb5d54c75ec074155de60
Dave Forgac <tylerdave@tylerdave.com>
2022-03-06 01:18:35 -0500

    update images

--- src/components/footer/footer.js
+++ src/components/footer/footer.js
@@ -74,7 +74,7 @@ const SiteFooter = () => {
         <div>
           <Link to="/about/ohpyo">
             <StaticImage
-              src="../../../content/assets/pyohio-2021-lightning-snake-sticker.png"
+              src="../../../content/assets/pyohio-lightning-snake-sticker.png"
               sx={{
                 // Styling for the logo using the breakpoint syntax from Theme-UI. Imagine something like [xs, sm, md, l, xl] for the breakpoint sizes. Play around with the logo sizes and your screen size to see the effects
                 height: ["30px", "30px", null, "30px", null],
--- src/gatsby-plugin-theme-ui/index.js
+++ src/gatsby-plugin-theme-ui/index.js
@@ -15,7 +15,7 @@ const theme = merge(BaseTheme, {
   // Modifications to the base theme go here. This is an example changing colors and using variants to change your navigation links. Uncomment the code below to see what happens.
   config: {
     useColorSchemeMediaQuery: false,
-    initialColorModeName: "dark",
+    initialColorModeName: "light",
     printColorModeName: "light,",
   },
   buttons: {
