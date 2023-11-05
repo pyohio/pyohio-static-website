87da41531d418e5aa3504ee5429ed59bb60fcdbe
Dave Forgac <dforgac@pyohio.org>
2023-11-05 12:02:10 -0500

    fix path issues

--- 2023/astro.config.mjs
+++ 2023/astro.config.mjs
@@ -5,7 +5,6 @@ import compress from 'astro-compress'
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://www.pyohio.org/2023/',
   base: '/2023',
   compressHTML: true,
   integrations: [
