d0401cfb61025c7ef7a6b0200954bbd76fde91e9
Dave Forgac <dforgac@pyohio.org>
2023-11-05 11:45:20 -0500

    configure base dir

--- 2023/astro.config.mjs
+++ 2023/astro.config.mjs
@@ -5,7 +5,8 @@ import compress from 'astro-compress'
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://www.pyohio.org/2023',
+  site: 'https://www.pyohio.org/2023/',
+  base: '/2023',
   compressHTML: true,
   integrations: [
     mdx(),
