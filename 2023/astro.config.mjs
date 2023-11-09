3c960ee2a2a64927d30c4dfc46a8dec13b6e43c9
Dave Forgac <dforgac@pyohio.org>
2023-11-08 21:39:33 -0500

    replace template meta with pyohio info (#226)
    * replace template meta with pyohio info
    
    * fix logo url
    
    * update title
    
    * add twitter card tags
    
    * update footer image
    
    * add title/description

--- 2023/astro.config.mjs
+++ 2023/astro.config.mjs
@@ -5,6 +5,7 @@ import compress from 'astro-compress'
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://www.pyohio.org/2023',
   base: '/2023',
   publicDir: './public-src',
   compressHTML: true,
