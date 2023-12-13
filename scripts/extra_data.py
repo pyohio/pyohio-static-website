8de4d68d3bcc252a4e5784230440b3dca368f895
Dave Forgac <dforgac@pyohio.org>
2023-12-13 16:07:44 -0500

    add codes to breaks

--- scripts/extra_data.py
+++ scripts/extra_data.py
@@ -11,36 +11,42 @@ TALK_EXTRAS = {
 
 BREAKS = {
     "BREAK1": {
+        "code": "BREAK1",
         "title": "Break",
         "start_time": "2023-12-16T10:55:00-05:00",
         "end_time": "2023-12-16T11:00:00-05:00",
         "type": "Break",
     },
     "BREAK2": {
+        "code": "BREAK2",
         "title": "Break",
         "start_time": "2023-12-16T11:53:00-05:00",
         "end_time": "2023-12-16T12:00:00-05:00",
         "type": "Break",
     },
     "BREAK3": {
+        "code": "BREAK3",
         "title": "Break",
         "start_time": "2023-12-16T12:54:00-05:00",
         "end_time": "2023-12-16T13:00:00-05:00",
         "type": "Break",
     },
     "BREAK4": {
+        "code": "BREAK4",
         "title": "Break",
         "start_time": "2023-12-16T13:54:00-05:00",
         "end_time": "2023-12-16T14:00:00-05:00",
         "type": "Break",
     },
     "BREAK5": {
+        "code": "BREAK5",
         "title": "Break",
         "start_time": "2023-12-16T14:55:00-05:00",
         "end_time": "2023-12-16T15:00:00-05:00",
         "type": "Break",
     },
     "BREAK6": {
+        "code": "BREAK6",
         "title": "Break",
         "start_time": "2023-12-16T15:53:00-05:00",
         "end_time": "2023-12-16T16:00:00-05:00",
