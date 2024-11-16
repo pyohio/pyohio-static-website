4a717d7fc872ceca61d5e51dcc0179bb443b1c75
Dave Forgac <dforgac@pyohio.org>
2024-11-16 16:15:06 -0500

    add base url path

--- blog/astro.config.mjs
+++ blog/astro.config.mjs
@@ -1,55 +1,71 @@
-import { defineConfig } from 'astro/config';
-import DecapCMS from 'astro-decap-cms';
+import { defineConfig } from "astro/config"
+import DecapCMS from "astro-decap-cms"
 
 // https://astro.build/config
 export default defineConfig({
+  base: "/blog",
   integrations: [
     DecapCMS({
       config: {
         // Use Netlify’s “Git Gateway” authentication and target our default branch
         backend: {
-          name: 'git-gateway',
-          branch: 'latest',
+          name: "git-gateway",
+          branch: "latest",
         },
         // Configure where our media assets are stored & served from
-        media_folder: 'public/assets/blog',
-        public_folder: '/assets/blog',
+        media_folder: "public/assets/blog",
+        public_folder: "/assets/blog",
         // Configure the content collections
         collections: [
           {
-            name: 'posts',
-            label: 'Blog Posts',
-            label_singular: 'Blog Post',
-            folder: 'src/pages/posts',
+            name: "posts",
+            label: "Blog Posts",
+            label_singular: "Blog Post",
+            folder: "src/pages/posts",
             create: true,
             delete: true,
             fields: [
-              { name: 'title', widget: 'string', label: 'Post Title' },
+              { name: "title", widget: "string", label: "Post Title" },
               {
-                name: 'publishDate',
-                widget: 'datetime',
-                format: 'DD MMM YYYY',
-                date_format: 'DD MMM YYYY',
+                name: "publishDate",
+                widget: "datetime",
+                format: "DD MMM YYYY",
+                date_format: "DD MMM YYYY",
                 time_format: false,
-                label: 'Publish Date',
+                label: "Publish Date",
               },
-              { name: 'author', widget: 'string', label: 'Author Name', required: false },
-              { name: 'authorURL', widget: 'string', label: 'Author URL', required: false },
-              { name: 'description', widget: 'string', label: 'Description', required: false },
-              { name: 'body', widget: 'markdown', label: 'Post Body' },
               {
-                name: 'layout',
-                widget: 'select',
-                default: '../../layouts/BlogPost.astro',
+                name: "author",
+                widget: "string",
+                label: "Author Name",
+                required: false,
+              },
+              {
+                name: "authorURL",
+                widget: "string",
+                label: "Author URL",
+                required: false,
+              },
+              {
+                name: "description",
+                widget: "string",
+                label: "Description",
+                required: false,
+              },
+              { name: "body", widget: "markdown", label: "Post Body" },
+              {
+                name: "layout",
+                widget: "select",
+                default: "../../layouts/BlogPost.astro",
                 options: [
-                  { label: 'Blog Post', value: '../../layouts/BlogPost.astro' },
+                  { label: "Blog Post", value: "../../layouts/BlogPost.astro" },
                 ],
               },
             ],
           },
         ],
       },
-      previewStyles: ['/src/styles/blog.css'],
+      previewStyles: ["/src/styles/blog.css"],
     }),
   ],
-});
+})
