f9a147b1045f6cfdd34699dd90f6f622863c05b2
Dave Forgac <dforgac@pyohio.org>
2024-11-16 23:21:43 -0500

    update admin path

--- blog/astro.config.mjs
+++ blog/astro.config.mjs
@@ -15,6 +15,7 @@ export default defineConfig({
         // Configure where our media assets are stored & served from
         media_folder: "public/assets/blog",
         public_folder: "/blog/assets/blog",
+        adminPath: "/blog/admin",
         // Configure the content collections
         collections: [
           {
