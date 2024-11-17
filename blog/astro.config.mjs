d20e92c3817594df5c61def5b94d0dd2249d7508
Dave Forgac <dforgac@pyohio.org>
2024-11-17 16:55:16 -0500

    fix icons

--- blog/astro.config.mjs
+++ blog/astro.config.mjs
@@ -4,9 +4,11 @@ import mdx from "@astrojs/mdx"
 
 import sitemap from "@astrojs/sitemap"
 
+import icon from "astro-icon";
+
 // https://astro.build/config
 export default defineConfig({
   site: "https://www.pyohio.org/blog",
   base: "/blog",
-  integrations: [mdx(), sitemap()],
+  integrations: [mdx(), sitemap(), icon()],
 })
