3958e52ffb1d00a3fc4822ca56115543ca2e84a1
Dave Forgac <dforgac@pyohio.org>
2024-11-17 01:09:47 -0500

    fix title and links

--- blog/src/consts.ts
+++ blog/src/consts.ts
@@ -1,5 +1,6 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Astro Blog';
-export const SITE_DESCRIPTION = 'Welcome to my website!';
+export const SITE_TITLE = "PyOhio Blog"
+export const SITE_DESCRIPTION =
+  "The blog for PyOhio, the free annual Python community conference in Ohio."
