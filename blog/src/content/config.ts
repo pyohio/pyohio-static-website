41546edc41f082b2b16ca5cd984cf556aa7c58ce
Dave Forgac <dforgac@pyohio.org>
2024-11-19 09:08:51 -0500

    add optiont to show/hide hero image

--- blog/src/content/config.ts
+++ blog/src/content/config.ts
@@ -10,6 +10,7 @@ const posts = defineCollection({
     pubDate: z.coerce.date(),
     updatedDate: z.coerce.date().optional(),
     showDate: z.boolean().optional(),
+    showHeroImage: z.boolean().optional(),
     heroImage: z.string().optional(),
   }),
 })
